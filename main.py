a1ec63040aa6facad6cc1d351564f62dd8ae4a58
JavohirJalilov <javohirjaliloff@gmail.com>
2023-10-07 18:45:23 +0500

    test

--- main.py
+++ main.py
@@ -1,16 +1,5 @@
-import json
+from likedb import LikeDB
 
-with open('data.json', 'r') as f:
-    data = json.loads(f.read())
+db = LikeDB("data.json")
 
-like = data['like']
-dislike = data['dislike']
-
-like += 1
-
-data['like'] = like
-
-json_data = json.dumps(data, indent=4)
-
-with open('data.json', 'w') as f:
-    f.write(json_data)
+print(db.add_dislike())
