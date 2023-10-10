ac1ee69347aa48274b01a5d174c0eb5963884d13
JavohirJalilov <javohirjaliloff@gmail.com>
2023-10-10 18:02:42 +0500

    add user

--- likedb.py
+++ likedb.py
@@ -8,27 +8,37 @@ class LikeDB:
             with open(path, 'r') as f:
                 self.data = json.load(f)
         except:
-            self.data = {
-                "like": 0,
-                "dislike": 0
-            }
+            self.data = {}
     
     def save(self, data):
         with open(self.path, 'w') as f:
             json.dump(data, f, indent=4)
     
+    def add_user(self, chat_id):
+        chat_id = str(chat_id)
+
+        
+        if chat_id not in self.data:
+            self.data[chat_id] = {
+                "like": 0,
+                "dislike": 0
+            }
+            self.save(self.data)
+
     def add_like(self, chat_id):
-        pass
+        self.data[str(chat_id)]['like'] += 1
+        self.save(self.data)
         
     
     def add_dislike(self, chat_id):
-        pass
+        self.data[str(chat_id)]['dislike'] += 1
+        self.save(self.data)
     
     def get_likes(self, chat_id) -> int:
-        pass
+        return self.data[str(chat_id)]['like']
 
     def get_dislikes(self, chat_id) -> int:
-        pass
+        return self.data[str(chat_id)]['dislike']
 
 
 
